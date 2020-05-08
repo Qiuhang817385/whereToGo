32414a0629bb1637d9084b47070120a714ef4155
Qiuhang817385 <826266129@qq.com>
2020-05-08 18:59:14 +0800

    init项目,准备暴露配置

---
+++ src/component/As.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './a.scss'
+export default class As extends Component {
+  render () {
+    return (
+      <div className="div1">
+        red
+      </div>
+    )
+  }
+}
